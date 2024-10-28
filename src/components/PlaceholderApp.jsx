95668d648f4c2fc8f84d7560f3eb06251169f485
BeTweak <ys@betweak.com>
2024-10-28 11:40:22 +0900

    setup bootstrap

---
+++ src/components/PlaceholderApp.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ListGroup, Badge } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
+export default function PlaceholderApp() {
+  return (
+    <Container>
+      <ListGroup as="ol" numbered>
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Subheading</div>
+            Cras justo odio
+          </div>
+          <Badge bg="primary" pill>
+            14
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Subheading</div>
+            Cras justo odio
+          </div>
+          <Badge bg="primary" pill>
+            14
+          </Badge>
+        </ListGroup.Item>
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">Subheading</div>
+            Cras justo odio
+          </div>
+          <Badge bg="primary" pill>
+            14
+          </Badge>
+        </ListGroup.Item>
+      </ListGroup>
+    </Container>
+  );
+}
